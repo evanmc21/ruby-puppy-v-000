16b7dbb7e052ae2ca839a984b512701e8736bc86
Evan Candler <emcandler21@gmail.com>
2018-02-26 23:15:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,8 +7,10 @@ class Dog
     @all << self
   end
   def self.all
-    @all.each { |do| puts dog.name}
+    @all.each {|do|
+      puts dog.name}
     end
+  end
   end
   def self.clear_all
     @all.clear
